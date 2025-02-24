8d8ed7d917e20860bef66e168c16cf85716aa92e
Benjamin Lukin <180255353+Benzux@users.noreply.github.com>
2025-02-24 02:32:50 +0200

    Added the Jupyter file for the model to the project (just in case), added a table showing the classification report

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from 'react'
 import pandasLogo from './assets/pandas_logo.png'
 import malLogo from './assets/mal.png'
 import './App.css'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function tableData(
+    desc: string | null,
+    precision: number | null,
+    recall: number | null,
+    f1_score: number | null,
+    support: number | null
+  ) {
+    return {desc, precision, recall, f1_score, support};
+  }
+
+  const rows = [
+    tableData('negative', 0.70, 0.69, 0.70, 3354),
+    tableData('neutral', 0.30, 0.07, 0.11, 1756),
+    tableData('positive', 0.86, 0.95, 0.91, 13118),
+    tableData('accuracy', null, null, 0.82, 18228),
+    tableData('macro avg', 0.62, 0.57, 0.57, 18228),
+    tableData('weighted avg', 0.78, 0.82, 0.79, 18228)
+  ]
+  
 
   return (
     <>
@@ -16,17 +41,47 @@ function App() {
           <img src={malLogo} className="logo mal" alt="MyAnimeList logo" />
         </a>
       </div>
-      <h1>Sentiment Analysis on MyAnimeList reviews using Pandas</h1>
+      <h2>Sentiment Analysis on MyAnimeList reviews using Pandas</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          This web application hosted on render.com serves as a visual tool for displaying the results of 
+          a sentiment analysis model trained on anime reviews obtained from the site MyAnimeList.
         </p>
-      </div>
-    </>
-  )
-}
+        <p>
+          The sentiment analysis was trained on over 85 thousand reviews sourced from
+           <a href="https://www.kaggle.com/datasets/stoicstatic/mal-top-10k-anime-details" target="_blank"> kaggle</a>.
+           Below is shown the evaluation of the trained model in the form of a classification report table and a confusion matrix.
+        </p>
+        </div>
+        <TableContainer component={Paper}>
+          <Table sx={{minWidth: 650}} aria-label="classification report table">
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell align='right'>Precision</TableCell>
+                <TableCell align='right'>Recall</TableCell>
+                <TableCell align='right'>F1-Score</TableCell>
+                <TableCell align='right'>Support</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.desc}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>{row.desc}</TableCell>
+                  <TableCell align='right'>{row.precision}</TableCell>
+                  <TableCell align='right'>{row.recall}</TableCell>
+                  <TableCell align='right'>{row.f1_score}</TableCell>
+                  <TableCell align='right'>{row.support}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        </>
+    )
+  }
 
 export default App
