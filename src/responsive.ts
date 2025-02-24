dd2e03c5df66203c381edafb4cb189f2da2544e6
Benjamin Lukin <180255353+Benzux@users.noreply.github.com>
2025-02-24 22:00:35 +0200

    responsiveness adjustments and padding plus comments

--- src/responsive.ts
+++ src/responsive.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+// Styled Components used in the UI, plus a const that determines what width constitutes as a "small screen", used for responsive UI
+
 export const ResponsiveSettings = {
     smallScreenMaxWidth: "550px"
 }
@@ -19,6 +21,7 @@ export const DataDiv = styled.div`
     max-width: max-content;
     align-items: center;
     justify-self: center;
+    padding: 1em;
 `
 
 export const TextParagraph = styled.p`
